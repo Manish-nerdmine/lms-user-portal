c02cd1293562bf5c531bdfa51245c0fa7965c197
Er-Rajnish-Kumar-Tiwari <tanish281202@gmail.com>
2025-10-26 13:57:50 +0000

    done wiht new changes in module name with fiexd

--- frontend/src/Components/UiPathExitScreen.jsx
+++ frontend/src/Components/UiPathExitScreen.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { FiExternalLink } from "react-icons/fi";
+import { useNavigate, useParams } from "react-router-dom";
 
 const UiPathExitScreen = () => {
+  const navigate = useNavigate();
+  
+  const { courseId } = useParams(); 
   return (
     <div className=" bg-gray-100/50 flex flex-col items-center justify-center pb-16">
       {/* Container for the Card and Mascot */}
@@ -46,7 +50,7 @@ const UiPathExitScreen = () => {
       {/* Close Session Button */}
       <button
         className="bg-[#048699] hover:bg-teal-600 text-white font-bold py-3 px-8 rounded-full shadow-lg transition duration-300 uppercase tracking-wider text-sm"
-        onClick={() => console.log("Session Closed")}
+        onClick={() => navigate(`/videos/${courseId}`)}
       >
         Close This Session
       </button>
